d8884934c07502c03d9f133f694d5a396657aeb8
orion160 <idbl64@outlook.com>
2020-09-04 01:27:16 -0500

    Add project files.

---
+++ Opengl_learn/Opengl_learn.cpp
@@ -0,0 +1,12 @@
+﻿// Opengl_learn.cpp : Defines the entry point for the application.
+//
+
+#include "Opengl_learn.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
