a792e7d940d0d120311388b4cf497ace9c2b2e1a
orion160 <idbl64@outlook.com>
2020-09-13 21:39:53 -0500

    Added example texture

--- 2-Modern_Opengl_hello_triangle/src/hello_triangle.cpp
+++ 2-Modern_Opengl_hello_triangle/src/hello_triangle.cpp
@@ -107,7 +107,6 @@ int main(int argc, char** argv)
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-
 	GLfloat vertices[] =
 	{
 		-0.5F, -0.5F, 0.0F,
@@ -122,7 +121,7 @@ int main(int argc, char** argv)
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), v.data(), GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
---
+++ 7-Texture_example/src/main.cpp
@@ -0,0 +1,145 @@
+// main.cpp : Defines the entry point for the application.
+//
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "shader_class.h"
+
+void framebuffer_size_callback(GLFWwindow* window, const int width, const int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main(int argc, char** argv)
+{
+	if (glfwInit() != GLFW_TRUE)
+	{
+		// Initialization failed
+		return -1;
+	}
+
+	constexpr int screen_width = 800;
+	constexpr int screen_height = 600;
+
+	GLFWwindow* window = glfwCreateWindow(screen_width, screen_height, "Texture example", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		// Window or OpenGL context creation failed
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+	{
+		std::cout << "Failed to initialize GLAD\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	std::cout << "GLFW-version: " << glfwGetVersionString() << '\n';
+	std::cout << "Opengl-version: " << glGetString(GL_VERSION) << '\n';
+
+	Shader shader("C:/dev/c++/Opengl_learn/7-Texture_example/resources/shaders/vertex/vertex_texture.shader", "C:/dev/c++/Opengl_learn/7-Texture_example/resources/shaders/fragment/fragment_texture.shader");
+
+	GLfloat vertices[] =
+	{
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+	};
+
+	GLuint indices[] =
+	{
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
+
+	// Bind VAO
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<GLvoid*>(0));
+	glEnableVertexAttribArray(0);
+
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<GLvoid*>(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// Texture attribute
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<GLvoid*>(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// load and create a texture
+	// -------------------------
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// load image, create texture and generate mipmaps
+	int width, height, nrChannels;
+	// The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+	unsigned char* data = stbi_load("C:/dev/c++/Opengl_learn/7-Texture_example/resources/images/container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	// Unbind VAO
+	glBindVertexArray(0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Bind texture
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		shader.use();
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	glfwTerminate();
+
+	return 0;
+}
